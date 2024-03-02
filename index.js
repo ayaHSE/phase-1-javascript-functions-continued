e3c45a516f6e528ed7e241e0b5972c989a2b9dd3
Aya Hassan <147654234+ayaHSE@users.noreply.github.com>
2024-03-02 15:10:13 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// Implementing the saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Implementing the mondayWork function
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Implementing the wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Testing the wrapAdjective function
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); //=> "You are !!!a dedicated programmer!!!"
+  
